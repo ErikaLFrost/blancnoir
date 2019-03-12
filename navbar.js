c6817fa634d29be3276e7e7c63b9dab2e0aa3122
ErikaLFrost <erika.loman@gmail.com>
2019-03-12 18:07:36 +0100

    Lagt in kommentarer i CSSen

--- navbar.js
+++ navbar.js
@@ -1,4 +1,4 @@
-
+//Kod för att ändra meny-länkarnas display från none till block när användaren trycker på hamburge-menyn
     function myFunction() {
       var x = document.getElementById("myLinks");
       if (x.style.display === "block") {
